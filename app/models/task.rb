0795f285c28a21ca419f14256df79ab00e9adcc7
Yogi <yogi@cyanlabsid.com>
2017-07-20 03:48:33 +0700

    integrate paypal

--- app/models/task.rb
+++ app/models/task.rb
@@ -21,4 +21,7 @@ class Task < ApplicationRecord
   has_attached_file :image, styles: {medium: "300x300>", thumb: "100x100>"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
+  def source_youtube(url)
+    url.split('=')[-1]
+  end
 end
