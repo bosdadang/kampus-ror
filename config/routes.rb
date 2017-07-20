d5344c44612576ef3e934ca29493f2705c466b42
Yogi <yogi@cyanlabsid.com>
2017-07-20 18:43:17 +0700

    -

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
 
   # get 'home/index'
   # root :to => "home#index"
@@ -12,10 +12,10 @@ Rails.application.routes.draw do
     member do
       get :subscrible
     end
-    resources :tasks, only:[:show]
+    resources :tasks, only: [:show]
   end
-    get "/my_courses", to:"courses#my_courses"
-    post "/payment_notification", to:"courses#payment_notification"
+  get "/my_courses", to: "courses#my_courses"
+  post "/payment_notification", to: "courses#payment_notification"
   # match '/payment_notification' => 'courses#payment_notification', :via => [:get, :post], :as => 'payment_notification'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
